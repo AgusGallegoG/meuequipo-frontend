ccd1419e7f4ea4a1f892916ffcb8361c33e8e945
AgusGallegoG <agus.verin7@gmail.com>
2025-06-17 20:34:39 +0200

    fix to interface response

--- src/sponsor/infrastructure/models/responses/ResponseSponsorFooter.ts
+++ src/sponsor/infrastructure/models/responses/ResponseSponsorFooter.ts
@@ -1,12 +1,12 @@
 import type { ResponseImage } from '@/shared/infrastructure/models/responses/ResponseImage';
 
-export type ResponseSponsorFooterList = {
+export interface ResponseSponsorFooterList {
   content: ResponseSponsor[];
-};
+}
 
-export type ResponseSponsor = {
+export interface ResponseSponsor {
   id: number;
   name: string;
   logo: ResponseImage | null;
   url: string;
-};
+}
